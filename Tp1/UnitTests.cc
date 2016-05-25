42dcc402c8e85d9db5e2b3feaa513e379cbad3ce
mbuzoruso <mbuzoruso@fi.uba.ar>
2016-05-25 14:59:34 -0300

    working on it...

--- Tp1/UnitTests.cc
+++ Tp1/UnitTests.cc
@@ -16,6 +16,12 @@
 #include "cmdline.cc"
 #include "DFTcalculator.h"
 #include <ctime>
+#include <assert.h>
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
 
 using namespace std;
 
@@ -29,6 +35,40 @@ static ostream *os = NULL;
 static fstream ifs; 		
 static fstream ofs;
 
+
+// trim from start (in place)
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+// trim from start (copying)
+static inline std::string ltrimmed(std::string s) {
+    ltrim(s);
+    return s;
+}
+
+// trim from end (copying)
+static inline std::string rtrimmed(std::string s) {
+    rtrim(s);
+    return s;
+}
+
+// trim from both ends (copying)
+static inline std::string trimmed(std::string s) {
+    trim(s);
+    return s;
+}
 
 class UnitTests
 {
@@ -45,58 +85,59 @@ private:
 	}
 
 public:
-	bool TestL2Distance1()
+	bool TestFTOfZeroesIsZero(string method="fft")
 	{
-		
+		string input = "0 0 0 0";
+		string output;
+		Calculate(input, output);
+		trim(input);
+		trim(output);
+		//imprimo data y result.
+		cout << "input: " << input << endl;
+		cout << "output: " << output << endl;
+		assert (output=="(0.00,0.00) (0.00,0.00) (0.00,0.00) (0.00,0.00)");
+		return true;
 	}
 
-	bool TestFTOfZeroesIsZero()
-	{
-
-	}
-
-	bool TestFTOf11(string algorithm)
-	{
-
-	}
-
-	bool TestFTOfii(string algorithm)
-	{
-
-	}
-
-	bool TestFTOfSum(string algorithm)
+	bool TestFTOf11(string method="fft")
 	{
 
 	}
 
-	bool TestFTOfScalar(string algorithm)
+	bool TestFTOfii(string method="fft")
 	{
 
 	}
 
-	bool TestFTofIFT(string algorithm)
+	bool TestFTOfSum(string method="fft")
 	{
 
 	}
 
-	bool TestIFTofFT(string algorithm)
+	bool TestFTOfScalar(string method="fft")
 	{
 
 	}
 
-	bool TestDual(string algorithm)
+	bool TestFTofIFT(string method="fft")
 	{
 
 	}
 
-	void Calculate(string input, string& output)
+	bool TestIFTofFT(string method="fft")
 	{
-		string method = "dft";
+
+	}
+
+	bool TestDual(string method="fft")
+	{
+
+	}
+
+	void Calculate(string input, string& output, string method="fft")
+	{
 		string line = input;
-
 		os = &cout;
-
 		//Creo un vector que almacenará la información leída
 		vector<complex> data = vector<complex>();
 		//Creo un vector que almacenará el resultado
@@ -124,19 +165,15 @@ public:
 			//Calculo la iFFT
 			DFTcalculator::calculateiFFT(data,result); 
 		}
-
 		//Imprimo el resultado
-        *os << std::setprecision(2);
-        *os << std::fixed;
-		*os << result;
-
 		std::ostringstream stream;
+		stream << setprecision(2);
+		stream << fixed;
 		stream << result;
 		std::string str =  stream.str();
 		const char* chr = str.c_str();
 		//cout << "Ouput:" << chr;
 		output = chr;
-
 
 		//imprimo data y result.
 		cout << "Longitud vector data: " << data.length() << endl;
@@ -151,15 +188,8 @@ public:
 int main(int argc, char** argv) 
 {
 	UnitTests* tests = new UnitTests();
-	string input = "1 1 1 0";
-	string output;
-	tests->Calculate(input, output);
-
-	//imprimo data y result.
-	cout << "input: " << input << endl;
-	cout << "output: " << output << endl;
+	cout << tests->TestFTOfZeroesIsZero();
 	return 0;
-
 }
 
 
