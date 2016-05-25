cfabd86e9a1258fe840dcb61f1b62a5bfc3f3d45
mbuzoruso <mbuzoruso@fi.uba.ar>
2016-05-25 15:53:45 -0300

    working on it

--- Tp1/UnitTests.cc
+++ Tp1/UnitTests.cc
@@ -36,39 +36,38 @@ static fstream ifs;
 static fstream ofs;
 
 
-// trim from start (in place)
+// función auxiliar ltrim
 static inline void ltrim(std::string &s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
 }
 
-// trim from end (in place)
+// función auxiliar rtrim
 static inline void rtrim(std::string &s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
 }
 
-// trim from both ends (in place)
+// función auxiliar trim
 static inline void trim(std::string &s) {
     ltrim(s);
     rtrim(s);
 }
 
-// trim from start (copying)
 static inline std::string ltrimmed(std::string s) {
     ltrim(s);
     return s;
 }
 
-// trim from end (copying)
 static inline std::string rtrimmed(std::string s) {
     rtrim(s);
     return s;
 }
 
-// trim from both ends (copying)
 static inline std::string trimmed(std::string s) {
     trim(s);
     return s;
 }
+
+
 
 class UnitTests
 {
